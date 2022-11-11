3e89fb7b1348018eb93913980bd3261432fd8aa4
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-11-11 14:12:02 +0200

    update Filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,14 +1,26 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { selectFilter } from 'redux/selectors';
+import { setFilter } from 'redux/filter/filterSlice';
 import { Label, Field } from './Filter.styled';
 
 export const Filter = () => {
+  const dispatch = useDispatch();
   const filter = useSelector(selectFilter);
+
+  useEffect(() => {
+    return () => dispatch(setFilter(''));
+  }, [dispatch]);
+
+  const handleChange = e => {
+    const { value } = e.target;
+    dispatch(setFilter(value));
+  };
 
   return (
     <Label>
       <span>Find contacts by name:</span>
-      <Field type="text" name="filter" value={filter} /* onChange={} */ />
+      <Field type="text" name="filter" value={filter} onChange={handleChange} />
     </Label>
   );
 };
